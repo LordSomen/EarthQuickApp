8858f282cd3eb637980e077899a196e2860a16b7
LordSomen <soumyajit637@gmail.com>
2017-08-14 11:20:23 +0530

    previous adding of fake data into array list is removed and now it is directly take the value from the json string of QueryUtils

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -31,15 +31,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations
-        ArrayList<EarthQuickInfo> earthquakes = new ArrayList<EarthQuickInfo>();
-        earthquakes.add(new EarthQuickInfo("7.5", "San Francisco", "15 sept,2017"));
-        earthquakes.add(new EarthQuickInfo("6.75", "London", "23 oct,2017"));
-        earthquakes.add(new EarthQuickInfo("4.566", "Tokyo", "23 july,2017"));
-        earthquakes.add(new EarthQuickInfo("5.66", "Mexico City", "12 august,2017"));
-        earthquakes.add(new EarthQuickInfo("3.2", "Moscow", "15 august,2017"));
-        earthquakes.add(new EarthQuickInfo("5.67", "Rio de Janeiro", "26 july,2017"));
-        earthquakes.add(new EarthQuickInfo("4.567", "Paris", "7 oct,2017"));
-
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 

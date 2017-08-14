4ed3b6592ccf2b3e02d31ef1be982456e0d44301
LordSomen <soumyajit637@gmail.com>
2017-08-14 11:15:21 +0530

    name of the current class changed

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -3,12 +3,12 @@ package com.example.android.quakereport;
  * Created by soumyajit on 12/8/17.
  */
 
-public class EarthQuickInfo {
+public class Earthquake {
 
     private String rate;
     private String place;
     private String dateTime;
-        public EarthQuickInfo(String rate , String place , String dateTime){
+        public Earthquake(String rate , String place , String dateTime){
             this.rate = rate;
             this.place = place;
             this.dateTime = dateTime;
